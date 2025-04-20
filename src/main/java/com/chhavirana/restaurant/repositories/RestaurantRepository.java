5f03e0a5908db425ebe65689e501c4faca422f95
Chhavi Rana <chhavirana83@gmail.com>
2025-04-21 05:04:15 +0530

    create restaurant entity and repository

---
+++ src/main/java/com/chhavirana/restaurant/repositories/RestaurantRepository.java
@@ -0,0 +1,10 @@
+package com.chhavirana.restaurant.repositories;
+
+import com.chhavirana.restaurant.domain.entities.Restaurant;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends ElasticsearchRepository<Restaurant, String> {
+    // TODO: Custom queries as in when needed
+}
