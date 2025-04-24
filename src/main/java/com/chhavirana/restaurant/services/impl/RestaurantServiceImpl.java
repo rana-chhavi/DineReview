0cb953157279f322aaeb56e6a99c384fcbc34a9c
Chhavi Rana <chhavirana83@gmail.com>
2025-04-24 07:06:02 +0530

    feat: implement restaurant delete functionality

--- src/main/java/com/chhavirana/restaurant/services/impl/RestaurantServiceImpl.java
+++ src/main/java/com/chhavirana/restaurant/services/impl/RestaurantServiceImpl.java
@@ -113,4 +113,9 @@ public class RestaurantServiceImpl implements RestaurantService {
 
         return restaurantRepository.save(restaurant);
     }
+
+    @Override
+    public void deleteRestaurant(String id) {
+        restaurantRepository.deleteById(id);
+    }
 }
